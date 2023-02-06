b08b4fe5810082028069c5cfcee4389d931da2c2
Vuong Dang <vuongdang@chidoanh.com>
2023-02-07 00:52:29 +0700

    user payment

--- src/pages/ca-nhan/so-thanh-toan/action.jsx
+++ src/pages/ca-nhan/so-thanh-toan/action.jsx
@@ -1,12 +1,19 @@
+import { Button } from '@/components/Button'
 import { Field } from '@/components/Field'
+import { Input } from '@/components/Input'
 import { Portal } from '@/components/Portal'
 import { Radio } from '@/components/Radio'
-import { PROFILE_TITLE_ID } from '@/config'
+import { Select } from '@/components/Select'
+import { PATH, PROFILE_TITLE_ID } from '@/config'
 import { useForm } from '@/hooks/useForm'
-import { required } from '@/utils'
+import { handleError, required } from '@/utils'
 import React, { useRef, useState } from 'react'
 import { Helmet } from 'react-helmet'
-
+import moment from 'moment'
+import { userService } from '@/services/user'
+import { useNavigate, useParams } from 'react-router-dom'
+import { useQuery } from '@/hooks/useQuery'
+import { Spin, message } from 'antd'
 
 const rules = {
     cardName: [required()],
@@ -17,14 +24,62 @@ const rules = {
 }
 
 export const ActionPaymentPage = () => {
-    const [step, setStep] = useState(0)
+    const { id } = useParams()
+    const [step, setStep] = useState(id ? 1 : 0)
     const typeRef = useRef('card')
     const form = useForm(rules)
+    const navigate = useNavigate()
+    const { data: paymentDetail, loading: getPaymentLoading } = useQuery({
+        enabled: !!id,
+        queryFn: () => userService.getPaymentDetail(id),
+        onSuccess: (res) => {
+            const t = res.data.expired.split('/')
+            const month = t[0]
+            const year = t[1]
+            form.setValues({
+                ...res.data,
+                month, year
+            })
+        },
+        onError: (err) => {
+            handleError(err)
+            navigate(PATH.Profile.Payment)
+        }
+    })
 
+    const { loading, refetch: actionService } = useQuery({
+        enabled: false,
+        queryFn: ({ params }) => {
+            if (id) {
+                return userService.editPayment(id, ...params)
+            } else {
+                return userService.addPayment(...params)
+            }
+        }
+    })
+
+
+    const onSubmit = async () => {
+        try {
+            if (form.validate()) {
+                await actionService({
+                    ...form.values,
+                    type: typeRef.current,
+                    expired: `${form.values.month}/${form.values.year}`
+                })
+
+                message.success(id ? 'Cập nhật thanh toán thành công' : 'Thêm sổ thanh toán thành công')
+
+                navigate(PATH.Profile.Payment)
+            }
+        } catch (err) {
+            handleError(err)
+        }
+    }
     return (
         <>
             <Portal selector={PROFILE_TITLE_ID}>
-                Add Debit / Credit Card
+                {id ? 'Edit Debit / Credit Card' : 'Add Debit / Credit Card'}
             </Portal>
             <Helmet>
                 <title>Add Debit / Credit Card</title>
@@ -61,77 +116,88 @@ export const ActionPaymentPage = () => {
 
                 {
                     step === 1 && (
-                        <div>
-                            <div className="row">
-                                <div className="col-12 col-md-6">
-                                    <Field
-                                        label="Card Number *"
-                                        placeholder="Card Number *"
-                                        {...form.register('cardNumber')}
-                                    />
-                                </div>
-                                <div className="col-12 col-md-6">
-                                    <Field
-                                        label="Name on Card *"
-                                        placeholder="Name on Card *"
-                                        {...form.register('cardName')}
-                                    />
-                                    
-                                </div>
-                                <div className="col-12">
-                                    {/* Label */}
-                                    <label>
-                                        Expiry Date *
-                                    </label>
-                                </div>
-                                <div className="col-12 col-md-4">
-                                    <div className="form-group">
-                                        <label className="sr-only" htmlFor="paymentMonth">Month</label>
-                                        <select className="custom-select" id="paymentMonth" required>
-                                            <option selected disabled value>Month *</option>
-                                            <option>January</option>
-                                            <option>February</option>
-                                            <option>March</option>
-                                        </select>
+                        <Spin spinning={getPaymentLoading}>
+                            <div>
+                                <div className="row">
+                                    <div className="col-12 col-md-6">
+                                        <Field
+                                            label="Card Number *"
+                                            placeholder="Card Number *"
+                                            {...form.register('cardNumber')}
+                                        />
+                                    </div>
+                                    <div className="col-12 col-md-6">
+                                        <Field
+                                            label="Name on Card *"
+                                            placeholder="Name on Card *"
+                                            {...form.register('cardName')}
+                                        />
+
+                                    </div>
+                                    <div className="col-12">
+                                        {/* Label */}
+                                        <label>
+                                            Expiry Date *
+                                        </label>
+                                    </div>
+                                    <div className="col-12 col-md-4">
+                                        <Field
+                                            {...form.register('month')}
+
+                                            renderField={(props) => <Select  {...props}>
+                                                <option>Month *</option>
+                                                {
+                                                    Array.from(Array(12)).map((_, i) => <option key={i} value={i + 1}>{moment(`${i + 1}/01/2000`).format('MMMM')}</option>)
+                                                }
+                                            </Select>}
+                                        />
+                                    </div>
+                                    <div className="col-12 col-md-4">
+                                        <Field
+                                            {...form.register('year')}
+                                            renderField={(props) => <Select {...props}>
+                                                <option>Year *</option>
+                                                {
+                                                    Array.from(Array(30)).map((_, i) => {
+
+                                                        const value = (new Date()).getFullYear() - 15 + i
+
+                                                        return <option value={value} key={value}>{value}</option>
+                                                    })
+                                                }
+                                            </Select>}
+                                        />
+                                    </div>
+                                    <div className="col-12 col-md-4">
+                                        <Field
+                                            placeholder="CVV"
+                                            {...form.register('cvv')}
+                                            renderField={props => <Input helper="The CVV Number on your credit card or debit card is a 3 digit number on VISA, MasterCard and Discover branded credit and debit cards." {...props} />}
+                                        />
+                                    </div>
+                                    <div className="col-12">
+                                        <Field
+                                            {...form.register('default')}
+                                            renderField={props => <div className="custom-control custom-checkbox mb-0">
+                                                <input onChange={(ev) => {
+                                                    if (paymentDetail && paymentDetail.data.default) {
+                                                        message.warning('Bạn không thể bỏ thanh toán mặc định')
+                                                    } else {
+                                                        props?.onChange?.(ev.target.checked)
+                                                    }
+                                                }} checked={props.value} type="checkbox" className="custom-control-input" id="defaultPaymentMethod" />
+                                                <label className="custom-control-label" htmlFor="defaultPaymentMethod">Default payment method</label>
+                                            </div>}
+                                        />
+
                                     </div>
                                 </div>
-                                <div className="col-12 col-md-4">
-                                    <div className="form-group">
-                                        <label className="sr-only" htmlFor="paymentCardYear">Year</label>
-                                        <select className="custom-select" id="paymentCardYear" required>
-                                            <option selected disabled value>Year *</option>
-                                            <option>2017</option>
-                                            <option>2018</option>
-                                            <option>2019</option>
-                                        </select>
-                                    </div>
-                                </div>
-                                <div className="col-12 col-md-4">
-                                    <div className="form-group">
-                                        <div className="input-group input-group-merge">
-                                            <input className="form-control" id="paymentCardCVV" type="text" placeholder="CVV *" required />
-                                            <div className="input-group-append">
-                                                <span className="input-group-text" data-toggle="popover" data-placement="top" data-trigger="hover" data-content="The CVV Number on your credit card or debit card is a 3 digit number on VISA, MasterCard and Discover branded credit and debit cards." data-original-title title>
-                                                    <i className="fe fe-help-circle" />
-                                                </span>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="col-12">
-                                    <div className="form-group">
-                                        <div className="custom-control custom-checkbox mb-3">
-                                            <input type="checkbox" className="custom-control-input" id="defaultPaymentMethod" />
-                                            <label className="custom-control-label" htmlFor="defaultPaymentMethod">Default payment method</label>
-                                        </div>
-                                    </div>
-                                </div>
+                                {/* Button */}
+                                <Button loading={loading} onClick={onSubmit} className="btn btn-dark" type="submit">
+                                    {id ? 'Edit Card' : "Add Card"}
+                                </Button>
                             </div>
-                            {/* Button */}
-                            <button className="btn btn-dark" type="submit">
-                                Add Card
-                            </button>
-                        </div>
+                        </Spin>
                     )
                 }
             </div>
